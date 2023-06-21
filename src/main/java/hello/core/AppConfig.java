aeb88e9697efa8bcf6febf97e0d6505c582f694b
herolove0424 <herolove0424@kakao.com>
2023-06-21 23:34:11 +0900

    06.21

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
@@ -11,11 +12,20 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
   public MemberService memberService(){
-    return new MemberServiceImpl(new MemoryMemberRepository());
+    return new MemberServiceImpl(memberRepository());
+  }
+
+  private static MemoryMemberRepository memberRepository() {
+    return new MemoryMemberRepository();
   }
 
   public OrderService orderService(){
-    return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    return new OrderServiceImpl(memberRepository(), discountPolicy());
+  }
+
+  private static DiscountPolicy discountPolicy() {
+    //return new FixDiscountPolicy();
+    return new RateDiscountPolicy();
   }
 
 }
